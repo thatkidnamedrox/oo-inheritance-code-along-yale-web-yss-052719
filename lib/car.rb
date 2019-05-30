27a76ba28c67687b3796b9e489ec2f3f42cce727
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-05-30 15:28:23 +0000

    Automatically backed up by Learn

--- lib/car.rb
+++ lib/car.rb
@@ -4,7 +4,7 @@ class Car
   attr_accessor :wheel_size, :wheel_number
   def initialize(wheel_size, wheel_number)
     @wheel_size = wheel_size
-    @wheel_number = number
+    @wheel_number = wheel_number
   end
 
   def go
