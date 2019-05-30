a49a92a97f4fc739f39ce239e6376ddb3d56fb1c
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-05-30 15:31:13 +0000

    Automatically backed up by Learn

--- lib/car.rb
+++ lib/car.rb
@@ -1,18 +1,9 @@
 require_relative "./vehicle.rb"
-class Car
-  @@all = []
-  attr_accessor :wheel_size, :wheel_number
-  def initialize(wheel_size, wheel_number)
-    @wheel_size = wheel_size
-    @wheel_number = wheel_number
-  end
+class Car < Vehicle
+  
 
   def go
     "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
-  end
-
-  def fill_up_tank
-    "filling up!"
   end
 
 end
